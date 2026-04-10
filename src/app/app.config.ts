0d9efefb9da4e3a928edf2ed9d42dbdb3c3d5448
Zahraa Salloum <zahraa.h.salloum@gmail.com>
2026-04-10 04:40:48 +0300

    Add scroll to top on navigation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@
  */
 
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
@@ -13,7 +13,10 @@ import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' })
+    ),
     provideHttpClient(
       withInterceptors([authInterceptor, errorInterceptor])
     ),
